5ff827e64bc3b4d7c5d5b6c47c38e664c826248e
Ale Pinheiro <alessandropsbra@gmail.com>
2025-03-14 16:24:30 -0300

    fix: updates component layout

--- src/webComponent.ts
+++ src/webComponent.ts
@@ -2,11 +2,7 @@ import { defineCustomElement } from 'vue'
 import App from '@/NoPaymentBanner.ce.vue'
 
 const NoPaymentBanner = defineCustomElement(App)
-customElements.define('no-payment-banner', NoPaymentBanner)
 
-export { NoPaymentBanner }
-
-customElements.define('no-payment-banner', NoPaymentBanner)
 export function register() {
   customElements.define('no-payment-banner', NoPaymentBanner)
 }
