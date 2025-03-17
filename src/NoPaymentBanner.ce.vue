7f0d338b92af2f689d5e5d872b7968ac267f2ed6
Ale Pinheiro <alessandropsbra@gmail.com>
2025-03-17 14:44:38 -0300

    fix: updates max wrapper size

--- src/NoPaymentBanner.ce.vue
+++ src/NoPaymentBanner.ce.vue
@@ -2,37 +2,62 @@
   <aside
     role="alert"
     :style="{
-      'background-color': '#d9d9d9',
-      transition: 'height 0.3s ease-in-out',
       overflow: 'hidden',
-      height: isOpen ? '4em' : '0',
+      backgroundColor: '#d9d9d9',
+      height: isOpen ? 'fit-content' : '0',
+      transition: 'height 0.3s ease-in-out',
     }"
   >
     <div
       :style="{
-        'max-width': '1280px',
-        margin: '0 auto',
-        display: 'flex',
-        'flex-direction': 'row',
-        'justify-content': 'space-between',
-        'align-items': 'center',
+        gap: '1em',
         height: '100%',
+        display: 'flex',
+        margin: '0 auto',
         padding: '0 1em',
+        maxWidth: '1200px',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
       }"
     >
-      <h2 :style="{ 'font-size': '22px' }">
+      <h2 :style="{ 'font-size': '18px', 'font-family': 'sans-serif', fontWeight: 'normal' }">
         <b>ATENÇÃO:</b>
-        A Bext não solicita pagamento <br />
+        A Bext não solicita pagamento
+        <br :style="{ display: windowWidth > 768 ? 'block' : 'none' }" />
         antecipado para aprovações de crédito!
       </h2>
       <button @click="isOpen = !isOpen" :style="{ 'background-color': '#d9d9d9', border: 'none' }">
-        x
+        <svg
+          :style="{
+            width: '1.3em',
+            height: '1.3em',
+          }"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 384 512"
+        >
+          <path
+            d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+          />
+        </svg>
       </button>
     </div>
   </aside>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 const isOpen = ref(true)
+const windowWidth = ref(window.innerWidth)
+const updateWidth = () => {
+  windowWidth.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth)
+})
 </script>
